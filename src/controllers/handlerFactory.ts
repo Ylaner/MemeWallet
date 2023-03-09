14a1d055090f022194826db0cc4fc77c857eacb2
Yasin Askarian <ylanerofficial@gmail.com>
2023-03-09 15:18:58 +0330

    remove /br from indexes

--- src/controllers/handlerFactory.ts
+++ src/controllers/handlerFactory.ts
@@ -5,27 +5,31 @@ export const createOne = async function (
   Model: any,
   type: string
 ) {
-  try{
-  const message = ctx.message!;
-  console.log(ctx.session.media);
-  const index = message.text?.toLowerCase().replace(/(\r\n|\n|\r)/gm, " ").split(" ");
+  try {
+    const message = ctx.message!;
+    console.log(ctx.session.media);
+    const index = message.text
+      ?.toLowerCase()
+      .replace(/(\r\n|\n|\r)/gm, " ")
+      .split(" ")
+      .filter((string) => string !== "");
 
-  await Model.create({
-    type: type,
-    userId: message.from.id,
-    mediaUniqueId: ctx.session.media?.file_unique_id,
-    mediaId: ctx.session.media?.file_id,
-    index: index,
-  });
-  ctx.session.step = "media";
-  ctx.session.media = undefined;
-  await ctx.reply(
-    `Done, You can find it via inline method @MemeWallet_bot ${index}` ,
-    {
-      reply_to_message_id: ctx.message?.message_id,
-    }
-  );
-  }catch(err) {
+    await Model.create({
+      type: type,
+      userId: message.from.id,
+      mediaUniqueId: ctx.session.media?.file_unique_id,
+      mediaId: ctx.session.media?.file_id,
+      index: index,
+    });
+    ctx.session.step = "media";
+    ctx.session.media = undefined;
+    await ctx.reply(
+      `Done, You can find it via inline method @MemeWallet_bot ${index}`,
+      {
+        reply_to_message_id: ctx.message?.message_id,
+      }
+    );
+  } catch (err) {
     console.log(err);
   }
 };
